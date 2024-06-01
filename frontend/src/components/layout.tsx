6e8be0646dfd3082491efd2efce527ef359f2fcc
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-06-01 17:03:36 +0200

    Simplified the header language.

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -6,8 +6,7 @@ export default function Layout({ children }: { children: any }) {
   return (
     <div className="background">
       <div className="header-banner">
-        Advanced to the next level with ❤️ at EthBelgrade 2024. Reach out to us
-        on <a href="https://t.me/+Y4NbIk81GAtiOGNk">telegram</a>.{' '}
+        Reach out to us on <a href="https://t.me/+Y4NbIk81GAtiOGNk">telegram</a>
       </div>
       <main className="mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-between pb-6 pt-16 sm:pb-24">
         <Logo />
