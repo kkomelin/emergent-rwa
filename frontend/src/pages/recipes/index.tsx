5d0c109a2905e4f19a0321479f69b2c8f92a709a
Poglavar Svemira <svemir@nftmagicians.xyz>
2024-06-01 16:06:38 +0200

    Fix wording of Recipe definition

--- frontend/src/pages/recipes/index.tsx
+++ frontend/src/pages/recipes/index.tsx
@@ -10,8 +10,8 @@ const RecipesIndexPage = () => {
       <p className="mb-8 text-neutral-500 dark:text-neutral-400">
         A Recipe is simply a collection of Attestation Schemas associated with an Expected Outcome.
         It is a blueprint/template suggesting users what Attestations they need to collect for a particular purpose.
-        Anyone can create a Recipe.
-        Below is the list of all Recipes currently existing. Click on a Recipe to see more details,
+        Given anyone can create a Recipe, they are not guarantees of any kind.
+        Below is the list of all Recipes that currently exist. Click on a Recipe to see more details,
         including the author, expected outcome, the list of Attestation Schemas it consists of, as well as
         the list of attestations that have been made using this recipe.
       </p>
